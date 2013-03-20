ea0a9ccb2c4105493e72df1ca53e9cf179fc6698
Gábor Egyed <egyed.gabor@mentha.hu>
2013-03-20 14:12:09 +0100

    initial commit

---
+++ config/mtDoctrineExtraPluginConfiguration.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * mtDoctrineExtraPlugin configuration.
+ *
+ * @author      Gábor Egyed <egyed.gabor@mentha.hu>
+ */
+class mtDoctrineExtraPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
